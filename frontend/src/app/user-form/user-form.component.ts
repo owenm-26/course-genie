a737ef3018a8a056f77d853365bcc7edf4898f8b
Wyatt <wnapier@bu.edu>
2024-08-07 19:46:22 -0400

    finished user-form basic info using template (not templateUrl)

--- frontend/src/app/user-form/user-form.component.ts
+++ frontend/src/app/user-form/user-form.component.ts
@@ -3,7 +3,32 @@ import { FormService } from '../form.service';
 
 @Component({
   selector: 'app-user-form',
-  templateUrl: './user-form.component.html',
+  // templateUrl: './user-form.component.html',
+  template: `
+    <form (ngSubmit)="onSubmit()">
+      <label for="name">Name:</label>
+      <input type="text" id="name" [(ngModel)]="formData.name" name="name" required>
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" [(ngModel)]="formData.email" name="email" required>
+
+      <label for="numCredits">Number of HUB credits desired:</label>
+      <select [(ngModel)]="formData.numCredits">
+        @for(val of creditOptions; track val) {
+          <option value={{val}}>{{ val }}</option>
+        }
+      </select>
+
+      <label for="desiredHubs">Choose the HUBs you need:</label>
+      <select [(ngModel)]="formData.desiredHubs" multiple>
+        @for(hub of hubOptions; track hub) {
+          <option value={{hub}}>{{ hub }}</option>
+        }
+      </select>
+
+      <button type="submit">Submit</button>
+    </form>
+  `,
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
@@ -13,6 +38,7 @@ export class UserFormComponent {
   'Ethical Reasoning', 'First-Year Writing Seminar', 'Global Citizenship', 'Historical Consciousness', 'Oral/Signed Communication',
   'Philosophical Inquiry', 'Quantitative Reasoning I', 'Quantitative Reasoning II', 'Research and Information', 'Scientific Inquiry I',
   'Scientific Inquiry II', 'Teamwork/Collaboration', 'Writing, Research, Inquiry', 'Writing-Intensive Course'];
+  creditOptions: number[] = [4, 8, 12, 16, 20];
 
   constructor(private formService: FormService) {}
 
