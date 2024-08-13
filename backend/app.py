b16158beb0bf85583c9e8f75a802e63c5d496747
Owen Mariani <mariani.owen@gmail.com>
2024-08-13 14:37:36 -0700

    updated .env and .gitignore

--- backend/app.py
+++ backend/app.py
@@ -90,4 +90,4 @@ def handle_form():
     return jsonify({"status": "success", "data": data})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=os.getenv('BACKEND_PORT'))
